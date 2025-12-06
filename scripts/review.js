3bb8634c9fef732bae2781bf09ca065aaf0653f7
Naison Mutongoreya <naisssmutongoreya@gmail.com>
2025-12-06 10:36:59 +0200

    product_review_form

---
+++ scripts/review.js
@@ -0,0 +1,8 @@
+let count = Number(localStorage.getItem("reviewCount")) || 0;
+
+count++;
+
+localStorage.setItem("reviewCount", count);
+
+document.getElementById("counter").textContent =
+    `You have submitted ${count} review(s).`;
